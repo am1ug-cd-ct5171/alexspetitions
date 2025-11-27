db3dd52da89974dd546cc49ab0272954c58be58c
am1ug-cd-ct5171 <A.Maurin1@universityofgalway.ie>
2025-11-27 21:51:09 +0000

    Added project structure

---
+++ src/test/java/com/ug/alexpetitions/PetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ug.alexpetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
